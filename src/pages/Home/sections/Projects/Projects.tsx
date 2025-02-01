bff1801aa1b196f3adef4b0ce87f970df15c6c01
BisNeT0 <bis_henrique@yahoo.com.br>
2025-02-01 15:57:28 -0300

    feat: add more assets and Projects

--- src/pages/Home/sections/Projects/Projects.tsx
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -8,60 +8,70 @@ import SlideImages from "../../../../components/SlideImages/SlideImages";
 // Lista de projetos
 const projects = [
     {
-        name: "Project 1",
-        duration: "Jul 2023 - Dez 2023",
+        name: "Tutorials Medicine and Nobre School",
+        duration: "Oct 3, 2024 - Dec 18, 2024",
         images: [
-            "/projects/tutorial.png",
-            "/projects/tutorial2.png",
-            "/projects/tutorial3.png",
-            "/projects/tutorial4.png",
-            "/projects/tutorial5.png",
-            "/projects/tutorial6.png",
-            "/projects/tutorial7.png",
-            "/projects/tutorial8.png",
+            "/projects/Tutorial.png",
+            "/projects/Tutorial2.png",
+            "/projects/Tutorial3.png",
+            "/projects/Tutorial4.png",
+            "/projects/Tutorial5.png",
+            "/projects/Tutorial6.png",
+            "/projects/Tutorial7.png",
+            "/projects/Tutorial8.png",
+            "/projects/Tutorial9.png",
+            "/projects/Tutorial10.png",
+            "/projects/Tutorial11.png",
         ], // Lista de imagens
-        description: "This is a brief description of Project 1. It involves building a full-stack application with React and Node.js.",
-        technologies: ["React", "Node.js", "MongoDB"],
-        projectLink: "https://example.com/project1",
-        codeLink: "https://github.com/example/project1"
+        description: "Redesigning the UI/UX for Medicine Tutorials and Nobre School, ensuring a fully responsive experience across smartphones and tablets.",
+        technologies: ["JavaScript", "HTML5", "CSS3"],
+        projectLink: "https://www.linkedin.com/posts/bisnet0_frontend-js-css-activity-7247601763212513281-KuBA?utm_source=share&utm_medium=member_desktop",
+        codeLink: "https://med.unef.edu.br/tutorial-professor"
     },
     {
-        name: "Project 2",
-        duration: "Jan 2023 - Mar 2023",
+        name: "Renova-me Church",
+        duration: "Oct 9, 2024 - Oct 16, 2024",
         images: [
-            "/projects/Projeto2.png",
-            "/projects/Projeto2_1.png",
-            "/projects/Projeto2_2.png",
-            "/projects/Projeto2_3.png",
-            "/projects/Projeto2_4.png",
-            "/projects/Projeto2_5.png",
-            "/projects/Projeto2_6.png",
+            "/projects/Renovame.png",
+            "/projects/Renovame1.png",
+            "/projects/Renovame2.png",
+            "/projects/Renovame3.png",
+            "/projects/Renovame4.png",
+            "/projects/Renovame5.png",
+            "/projects/Renovame6.png",
+            "/projects/Renovame7.png",
+            "/projects/Renovame8.png",
         ], // Lista de imagens
-        description: "This project focuses on creating a mobile application using React Native for iOS and Android.",
-        technologies: ["React Native", "Redux", "Firebase"],
-        projectLink: "https://example.com/project2",
-        codeLink: "https://github.com/example/project2"
+        description: "This project was focused on 'pro bono publico', creating a fully responsive website for a new church called Renova-me Church.",
+        technologies: ["JavaScript", "HTML5", "CSS3"],
+        projectLink: "https://renovamechurch.com.br/",
+        codeLink: "https://github.com/bisnet0/renova-me"
     },
     {
-        name: "Project 3",
-        duration: "Jan 2023 - Mar 2023",
+        name: "Facial Biometrics UNEF and UNIFAN",
+        duration: "Jul 29, 2024 - Aug 6, 2024",
         images: [
-            "/projects/Projeto3.png",
-            "/projects/Projeto3_1.png",
-            "/projects/Projeto3_2.png",
-            "/projects/Projeto3_3.png",
-            "/projects/Projeto3_4.png",
-            "/projects/Projeto3_5.png",
-            "/projects/Projeto3_6.png",
+            "/projects/Biometria.png",
+            "/projects/Biometria1.png",
+            "/projects/Biometria2.png",
+            "/projects/Biometria3.png",
+            "/projects/Biometria4.png",
+            "/projects/Biometria5.png",
+            "/projects/Biometria6.png",
+            "/projects/Biometria7.png",
+            "/projects/Biometria8.png",
+            "/projects/Biometria9.png",
+            "/projects/Biometria10.png",
+            "/projects/Biometria11.png",
         ], // Lista de imagens
-        description: "This project focuses on creating a mobile application using React Native for iOS and Android.",
-        technologies: ["React Native", "Redux", "Firebase"],
-        projectLink: "https://example.com/project2",
-        codeLink: "https://github.com/example/project2"
+        description: "This project was developed to address the turnstile access issue at the two institutions of Grupo Nobre. It involved sending students and employees photos to authorize their entry.",
+        technologies: ["Python", "Node.js", "SQL Server","JavaScript", "HTML5", "CSS3"],
+        projectLink: "https://www.linkedin.com/posts/bisnet0_frontend-webdeb-javascript-activity-7227072365472448514-m9m7?utm_source=share&utm_medium=member_desktop",
+        codeLink: "https://biometria.unef.edu.br/",
     },
     {
-        name: "Project 4",
-        duration: "Jan 2023 - Mar 2023",
+        name: "Monthly Birthdays",
+        duration: "Jun 26, 2024 - Jul 23, 2024",
         images: [
             "/projects/Aniversariantes.png",
             "/projects/Aniversariantes1.png",
@@ -69,46 +79,47 @@ const projects = [
             "/projects/Aniversariantes3.png",
             "/projects/Aniversariantes4.png",
         ], // Lista de imagens
-        description: "This project focuses on creating a mobile application using React Native for iOS and Android.",
-        technologies: ["React Native", "Redux", "Firebase"],
-        projectLink: "https://example.com/project2",
-        codeLink: "https://github.com/example/project2"
+        description: "This project resulted in the creation of a Birthday Reminder for all Grupo Nobre employees. It displays all birthdays and includes a special celebration feature for the employee celebrating their birthday that day.",
+        technologies: ["JavaScript","Node.js", "SQL Server", "HTML5", "CSS3"],
+        projectLink: "https://www.linkedin.com/posts/bisnet0_webdev-desenvolvimentoweb-nodejs-activity-7220542341504245760-yEhG?utm_source=share&utm_medium=member_desktop"
+        // codeLink: "https://github.com/bisnet0/funcionarios-app"
     },
     {
-        name: "Project 5",
-        duration: "Jan 2023 - Mar 2023",
+        name: "Polos Management",
+        duration: "Jan 17, 2025 - Jan 29, 2025",
         images: [
-            "/projects/1_polo.png",
-            "/projects/2_polo.png",
-            "/projects/3_polo.png",
-            "/projects/polo.png",
-            "/projects/polo2.png",
-            "/projects/polo3.png",
-            "/projects/polo4.png",
-            "/projects/polo5.png",
-            "/projects/polo6.png",
+            "/projects/Polo.png",
+            "/projects/Polo1.png",
+            "/projects/Polo2.png",
+            "/projects/Polo3.png",
+            "/projects/Polo4.png",
+            "/projects/Polo5.png",
+            "/projects/Polo6.png",
+            "/projects/Polo7.png",
+            "/projects/Polo8.png",
+            "/projects/Polo9.png",
         ], // Lista de imagens
-        description: "This project focuses on creating a mobile application using React Native for iOS and Android.",
-        technologies: ["React Native", "Redux", "Firebase"],
-        projectLink: "https://example.com/project2",
-        codeLink: "https://github.com/example/project2"
+        description: "This project tackled extracting data from Brazil's complex ERP system (TOTVS RM) and centralizing it into a responsive web app, enabling real-time data access for all UNEF College campuses.",
+        technologies: ["React", "JSX", "SQL Server", "MongoDB", "Node.js", "JWT"],
+        projectLink: "https://www.linkedin.com/posts/bisnet0_react-nodejs-desenvolvimentoweb-activity-7287959265695166465-Gum8?utm_source=share&utm_medium=member_desktop",
+        codeLink: "https://polos.unef.edu.br/"
     },
     {
-        name: "Project 6",
-        duration: "Jan 2023 - Mar 2023",
+        name: "Portal Customizer",
+        duration: "Jun 16, 2024 - Jun 28, 2024",
         images: [
             "/projects/replaceM.png",
             "/projects/replaceM1.png",
             "/projects/replaceM2.png",
         ], // Lista de imagens
-        description: "This project focuses on creating a mobile application using React Native for iOS and Android.",
-        technologies: ["React Native", "Redux", "Firebase"],
-        projectLink: "https://example.com/project2",
-        codeLink: "https://github.com/example/project2"
+        description: "This project focuses on creating a costumizable automatic replacement tool for a Grupo Nobre Academic Portal's pages.",
+        technologies: ["Python", "HTML5", "CSS3"],
+        projectLink: "https://www.linkedin.com/posts/bisnet0_boa-tarde-rede-tudo-bem-com-voc%C3%AAs-bom-activity-7209987530543296512-V1ar?utm_source=share&utm_medium=member_desktop",
+        codeLink: "https://github.com/bisnet0/Replace_RM_Folder"
     },
     {
-        name: "Project 7",
-        duration: "Jan 2023 - Mar 2023",
+        name: "Predictive Tax Analysis",
+        duration: "Jun 18, 2024 - Jun 19, 2024",
         images: [
             "/projects/APreditiva.png",
             "/projects/APreditiva1.png",
@@ -116,10 +127,25 @@ const projects = [
             "/projects/APreditiva3.png",
             "/projects/APreditiva4.png",
         ], // Lista de imagens
-        description: "This project focuses on creating a mobile application using React Native for iOS and Android.",
-        technologies: ["React Native", "Redux", "Firebase"],
-        projectLink: "https://example.com/project2",
-        codeLink: "https://github.com/example/project2"
+        description: "This project focuses on creating a Predictive Analysis tool for Brazilian import and export taxes. It concentrates on data from the year range 2000 to 2024.",
+        technologies: ["Python", "Jupyter", "Pamdas","Scikit-learn", "Matplotlib", "Numpy"],
+        // projectLink: "https://example.com/project2",
+        codeLink: "https://github.com/bisnet0/DataScience"
+    },
+    {
+        name: "Nobre Events Hub",
+        duration: "Aug 14, 2024 - Sep 2, 2024",
+        images: [
+            "/projects/Nobrehub.png",
+            "/projects/Nobrehub1.png",
+            "/projects/Nobrehub2.png",
+            "/projects/Nobrehub3.png",
+            "/projects/Nobrehub4.png",
+        ], // Lista de imagens
+        description: "This project focuses on creating a centralized hub for all Nobre Group events (free and paid with Mercado Pago API). It is designed to manage event information and generate certificates for participants.",
+        technologies: ["JavaScript", "MercadoPago API", "Python","Node.js"],
+        projectLink: "https://www.linkedin.com/posts/bisnet0_development-system-backend-activity-7264752938651185152-9vcq?utm_source=share&utm_medium=member_desktop",
+        // codeLink: "https://github.com/bisnet0/DataScience"
     },
 ];
 
